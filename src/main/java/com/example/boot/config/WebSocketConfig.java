607c85667101cbdbad159a39981a6324895114e8
NinhPc <ninhmphpl@gmail.com>
2023-02-07 20:30:05 +0700

    commit 1

--- src/main/java/com/example/boot/config/WebSocketConfig.java
+++ src/main/java/com/example/boot/config/WebSocketConfig.java
@@ -9,23 +9,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker //>> được dùng để bật tính năng WebSocket server
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer { //>> import nó
-    /**
-     * Trong phương thức đầu tiên, chúng ta đăng ký một websocket endpoint mà các máy khách sẽ sử dụng để kết nối với máy chủ websocket của chúng ta.
-     * Lưu ý việc sử dụng withSockJS() với endpoint configuration. SockJS được sử dụng để bật tùy chọn dự phòng cho các trình duyệt không hỗ trợ websocket.
-     */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        //>> địa chỉ đich mà client sẽ gửi >> localhost:8080/socket
+        registry.addEndpoint("/socket")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
-
-    /**
-     * định nghĩa rằng các thông điệp có đích bắt đầu bằng “/topic” nên được định tuyến tới nhà môi giới tin nhắn.
-     * Nhà môi giới tin nhắn sẽ phát các tin nhắn đến tất cả các khách hàng được kết nối đã đăng ký một chủ đề cụ thể.
-     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app")
+                .enableSimpleBroker("/message");
     }
-
 }
